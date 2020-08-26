0be6ab571ca5614c8c5ee934bb8c900d52e7c95b
tundeiness <tundeiness@yahoo.co.uk>
2020-08-26 13:12:36 +0100

    implement input

--- src/components/dashboard/dashboard.js
+++ src/components/dashboard/dashboard.js
@@ -7,24 +7,41 @@ import './dashboard.css';
 class Dashboard extends React.Component {
   state = {
     showForm: false,
-    showDiv: false
+    showDiv: false,
+    name: '',
   };
 
-  postQuizName = () => {
+  getInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    console.log(value)
+    this.setState({
+      [name]: value
+    });
+  }
+
+  postQuizName = (quiz) => {
     // post the name gotten from the input to the API
 
-      const url = "/api/v1/quiz";
-      fetch(url)
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          }
-          throw new Error("Network response was not ok.");
-        })
-        .then(
-          // response => getDbCocktails(response.data)
-        )
-        .catch(error => error);
+    const URL = "https://serene-mountain-66508.herokuapp.com/api/v1/quizzes";
+
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+    fetch(URL, {
+      method: "POST",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({quiz: { Title: quiz }})
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('This Quiz exist in your list, choose another one');
+      })
+      // .then(()=> data)
+      .catch(error =>  error );
 
   }
 
@@ -35,14 +52,16 @@ class Dashboard extends React.Component {
   }
 
   showForm = () => {
+    const {name} = this.state
+
      return (
        <div>
         <form>
           <div className="px-md-5 form-group">
             <h6>Create Quiz:</h6>
-            <input type="text" id="quizname" className="form-control" placeholder="Quiz Name" />
+            <input type="text" id="quizname" name='name' value ={name} className="form-control" placeholder="Quiz Name" onChange={(e)=> this.getInput(e)} />
              <br/>
-            <button onClick={() => this.setState({showForm: false, showDiv: true}) && this.postQuizName()}>Create</button>
+            <button onClick={() => this.setState({showForm: false, showDiv: true}) && this.postQuizName(name)}>Create</button>
           </div>
           </form>
         </div>
@@ -54,15 +73,15 @@ class Dashboard extends React.Component {
       <div>
         <nav className="navbar navbar-expand-md navbar-light bg-light">
           <Link to="/register" className="btn btn-link white"><h2>NowLedge</h2></Link>
-          <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-              <span class="navbar-toggler-icon"></span>
+          <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+              <span className="navbar-toggler-icon"></span>
           </button>
 
-          <div class="collapse navbar-collapse" id="navbarCollapse">
-              <div class="navbar-nav">
+          <div className="collapse navbar-collapse" id="navbarCollapse">
+              <div className="navbar-nav">
                   <Link to="/" className="nav-item nav-link active">Home</Link>
               </div>
-              <div class="navbar-nav ml-auto">
+              <div className="navbar-nav ml-auto">
                   <Link to="/login" className="nav-item nav-link">Login</Link>
                   <Link to="/register" className="nav-item nav-link">Register</Link>
               </div>
@@ -71,8 +90,8 @@ class Dashboard extends React.Component {
         <br />
 
         <main className="px-md-5">
-          <div class="d-flex bd-highlight">
-            <div class="p-2 w-50 bd-highlight">
+          <div className="d-flex bd-highlight">
+            <div className="p-2 w-50 bd-highlight">
               <h2>Take your students'</h2>
               <h2>success to the next level</h2>
               <p>NowLedge helps teachers engage</p>
@@ -80,8 +99,8 @@ class Dashboard extends React.Component {
               <p>curriculum-based materials,</p>
               <p>interactive study methods and games.</p>
             <div className="row mx-md-n5">
-              <div className="col px-md-5"><button class="p-3 border bg-light" onClick={() => this.setState({showForm: true}) }>I'm a teacher</button></div>
-              <div className="col px-md-5"><button class="p-3 border bg-light">I'm a student</button></div>
+              <div className="col px-md-5"><button className="p-3 border bg-light" onClick={() => this.setState({showForm: true}) }>I'm a teacher</button></div>
+              <div className="col px-md-5"><button className="p-3 border bg-light">I'm a student</button></div>
             </div>
             </div>
             <div className="p-2 flex-shrink-1 bd-highlight">
