c30fa07133d2cc8323f7d1f5d16065e94f5678cb
oluwaseun bello <bellomsean@gmail.com>
2020-08-26 06:34:40 +0100

    updated login.js

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,43 +1,68 @@
 /* eslint-disable react/no-unused-state */
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import { userActions } from '../../actions';
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-
-    
-
+    // this.props.logout();
+    this.state = {
+      email: '',
+      password: '',
+      submitted: false
     };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.submitLogin = this.submitLogin.bind(this);
+    
+  }
+ 
+  handleInputChange(e) {
+    let name = e.target.name;
+    let value = e.target.value;
+    this.setState({
+      [name]: value
+    });
+  }
+ 
+  submitLogin(e) {
+    e.preventDefault();
+    this.setState({ submitted: true });
+    const { email, password } = this.state;
+    if (email && password) {
+      this.props.login(email, password);
+    }
+  }
 
   render() {
-
+    // const { loggingIn } = this.props;
+    const { email, password, submitted } = this.state;
     return (
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-4" />
-          <div className="col-md-4 mt-5">
-            <h2 className="text-center">Login To Continue</h2>
-            <form>
-              <div className="form-group">
-                <h6>Username:</h6>
-                <input type="text" id="firstName" className="form-control input-shadow" placeholder="Enter Username"  name="firstName" />
-                 <div className="help-block">Username is required</div>
-              </div>
-              <div className="form-group">
-                <h6>Password:</h6>
-                <input type="text" id="lastName" className="form-control input-shadow" placeholder="Enter Password" name="lastName" />
-                <div className="help-block">Password is required</div>
-              </div>
-
-              <button type="button"  className="btn btn-primary btn-block">Login</button>
-              <Link to="/register" className="btn btn-link">Register</Link>
-            </form>
+        <div className="col-md-4" />
+          <div className="col-md-4 col-md-offset-4">
+            <h2 className="text-center">User Login</h2>
+              <form name="form">
+                <div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
+                  <label for="email">Email:</label>
+                  <input type="text" id="email" className="form-control input-shadow" placeholder="Enter Email" value={this.state.email} onChange={this.handleInputChange} name="email"/>
+                  {submitted && !email && <div className="help-block">Email is required</div> }
+                </div>
+                <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
+                  <label>Password: </label>
+                  <input type="password" id="exampleInputPassword" className="form-control input-shadow" placeholder="Enter Password" value={this.state.password} onChange={this.handleInputChange} name="password"/>
+                  {submitted && !email && <div className="help-block">Password is required</div> }
+                </div>
+                
+                <button type="button" onClick={this.submitLogin} className="btn btn-primary btn-block">Sign In</button>
+                <Link to="/register" className="btn btn-link">Register</Link>
+              </form>
           </div>
-          <div className="col-md-4" />
         </div>
       </div>
-    );
+    )
   }
 }
 
